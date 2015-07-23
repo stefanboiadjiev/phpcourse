079a8b729ac4bbf723cd7d629c2a7009a257be23
stefan <stefan_boiadjiev@mail.bg>
2015-07-23 21:50:07 +0300

    practice php

---
+++ 04-php-basics/index.php
@@ -0,0 +1,30 @@
+<?php
+	$my_name1 = "Stefan";
+	$my_name2 = "Boiadjiev";
+	$datata = "23";
+	$a = "23";
+	$b = "25";
+	
+	echo $my_name1 . " " . $my_name2;
+	echo "<br/>";
+	
+	if ($datata == "25" ||  $datata == "25" )
+	{
+		echo "Nema pari";
+	}
+	else 
+	{
+		echo "Ima pari";
+	}				
+
+	echo "<br/>";
+	
+	echo "a= $a" . "  " . "b= $b";
+	$a = $a + $b;
+	echo "<br/>";
+	echo "sumata im e $a";
+	$b = $b++;
+	echo "<br/>";
+	echo "uvelichihme b s 1 i poluchihme $b";
+	
+?>
