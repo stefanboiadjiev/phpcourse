bdcf64eb3abf8d0b2ef5a7f7e278d5dac5f5d947
stefan <stefan_boiadjiev@mail.bg>
2015-07-28 19:55:17 +0300

    php home

---
+++ 04-php-home/index.php
@@ -0,0 +1,24 @@
+
+<?php
+	
+	
+	$a = 35;
+
+	if ($a ==15 )
+	{
+		echo "аз съм 15";
+	}
+	if ($a > 26) 
+	{
+		echo "<br />";
+		echo "по-голямо  от 25";
+	} 
+	
+	if ($a <= 64) 
+	{
+		echo "<br />";
+		echo '<body style="background-color:red">';
+		echo "по-малко или равно на 64";
+	}
+	
+ ?>
